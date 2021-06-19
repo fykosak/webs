ffa9b4791551de537470653c368c585b3d1db67f
vanekm <martin.vanek42@gmail.com>
2021-06-19 15:57:13 +0200

    update rules

--- app/Modules/DefaultModule/RulesPresenter.php
+++ app/Modules/DefaultModule/RulesPresenter.php
@@ -7,12 +7,17 @@ class RulesPresenter extends BasePresenter {
 
     public function renderDefault(): void
     {
-        $this->setPagetitle(_('Rules'));
+        $this->setPagetitle(_('Brief Rules'));
         $this->changeViewByLang();
     }
 
     public function renderComplete(){
         $this->setPagetitle(_('Full Rules'));
+        $this->changeViewByLang();
+    }
+
+    public function renderOrganisationalRegulations(){
+        $this->setPagetitle(_('Organisational Regulations'));
         $this->changeViewByLang();
     }
 }
