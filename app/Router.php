35795880e0272a5655a30a982df4ba7e13514d60
tuaki <jachymb32@gmail.com>
2021-06-19 18:14:54 +0200

    router update and archive presenter

--- app/Router.php
+++ app/Router.php
@@ -10,32 +10,14 @@ class Router {
     public static function createRouter(): \Nette\Routing\Router {
         $router = new RouteList();
 
-        $router->addRoute('<presenter>[/<action>]', [
-            'module' => 'Default',
-            'presenter' => 'Default',
-            'action' => 'default'
-        ]);
-
-        $router[] = new Route('index.php', [
-            'module' => 'Frontend',
-            'presenter' => 'Default',
-            'action' => 'default',
-        ], Route::ONE_WAY);
-        $router[] = new Route('<eventYear [0-9]+>[/<presenter>[/<action>]]', [
-            'module' => 'Archive',
-            'presenter' => 'Default',
-            'action' => 'default',
-        ]);
-        $router[] = new Route('team/[<action>/[<id>]]', [
-            'module' => 'Default',
-            'presenter' => 'Team',
-            'action' => 'default',
-        ]);
-        $router[] = new Route('[<action>/[<id>]]', [
-            'module' => 'Default',
-            'presenter' => 'Default',
-            'action' => 'default',
-        ]);
+        $router
+            ->withModule('Default')
+                ->addRoute('index.php', 'Default:default', $router::ONE_WAY)
+                ->addRoute('<presenter>[/<action>]', 'Default:default')
+                ->addRoute('team/[<action>/[<id>]]', 'Team:default')
+            ->withModule('Archive')
+                ->addRoute('<eventYear [0-9]+>[/<presenter>[/<action>]]', 'Default:default');
+        
         return $router;
     }
 }
