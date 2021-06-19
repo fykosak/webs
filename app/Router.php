079dcdd9d65e2e08bd26756074114bd2e680fdc2
Štěpán Stenchlák <s.stenchlak@gmail.com>
2021-06-19 23:09:03 +0200

    Implemented router address translation

--- app/Router.php
+++ app/Router.php
@@ -2,12 +2,66 @@
 
 namespace App;
 
+use Nette\Application\BadRequestException;
 use Nette\Application\Routers\Route;
 use Nette\Application\Routers\RouteList;
 
 class Router {
 
-    public static function createRouter(): \Nette\Routing\Router {
+    /**
+     * Creates a global filter for a route modifying parameters to use domains instead of lang.
+     * @param array|null $domainList
+     * @param array $routerMapping
+     * @param string $key todo replace by an array of keys
+     * @return \Closure[]
+     */
+    private static function useTranslateFilter(?array $domainList, array $routerMapping, string $key): array {
+        return [
+            Route::FILTER_IN => function (array $params) use($routerMapping, $domainList, $key): array {
+                if ($domainList && count($domainList)) {
+                    $domainLang = $domainList[$params['domain']] ?? null;
+                    if ($domainLang === null) {
+                        trigger_error('Domain \'' . $params['domain'] . '\' has no language assigned. Fallback to en.', E_USER_WARNING);
+                        $domainLang = 'en';
+                    }
+
+                    if ($domainLang !== 'en') {
+                        if (array_key_exists($params['presenter'], $routerMapping[$domainLang])) {
+                            $params['lang'] = $domainLang;
+                            $params[$key] = $routerMapping[$domainLang][$params[$key]];
+                        } else {
+                            // 404 because there is no translation
+                            throw new BadRequestException(404);
+                        }
+                    }
+                }
+                return $params;
+            },
+
+            // From params to URL
+            Route::FILTER_OUT => function (array $params) use($routerMapping, $domainList, $key): array {
+                if ($domainList && count($domainList)) {
+                    if ($params['lang'] !== 'en') {
+                        $params[$key] = array_search($params[$key], $routerMapping[$params['lang']]);
+                    }
+
+                    $params['domain'] = array_search($params['lang'], $domainList);
+                    if ($params['domain'] === false) {
+                        // No suitable domain for specified lang
+                        $params['domain'] = array_key_first($domainList);
+                    }
+                    unset($params['lang']);
+                } else {
+                    $params['domain'] = $_SERVER['HTTP_HOST'];
+                }
+
+
+                return $params;
+            },
+        ];
+    }
+
+    public static function createRouter(?array $domainList, array $routerMapping): \Nette\Routing\Router {
         $router = new RouteList();
 
         $router->withModule('Archive')
@@ -15,14 +69,15 @@ class Router {
 
         $router->withModule('Default')
             ->addRoute('index.php', 'Default:default', $router::ONE_WAY)
-            ->addRoute('<presenter>[/<action>]', [
+            ->addRoute('//<domain>/<presenter>[/<action>]', [
                 'presenter' => [
                     Route::VALUE => 'Default',
                     Route::FILTER_TABLE => [
-                        'about' => 'AboutTheCompetition',
-                    ],
+                        'about' => 'AboutTheCompetition'
+                    ]
                 ],
                 'action' => 'default',
+                null => self::useTranslateFilter($domainList, $routerMapping, "presenter")
             ])
             ->addRoute('team/[<action>/[<id>]]', 'Team:default');
 
