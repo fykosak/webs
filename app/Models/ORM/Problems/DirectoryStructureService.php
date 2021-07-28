8d35ceff483ed6a8f17bdb3db4309d47aa472987
Michal Mišo Červeňák <miso@fykos.cz>
2021-07-29 00:19:02 +0200

    update ORM

--- app/Models/ORM/Problems/DirectoryStructureService.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Models\ORM\Problems;
-
-use Fykosak\NetteORM\AbstractModel;
-
-class DirectoryStructureService extends AbstractModel {
-
-}
--- app/Models/ORM/Problems/ProblemService.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Models\ORM\Problems;
-
-use Fykosak\NetteORM\AbstractService;
-
-class ProblemService extends AbstractService {
-
-}
