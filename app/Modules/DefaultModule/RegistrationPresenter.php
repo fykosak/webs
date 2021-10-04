50dfe9662c6b5c9b21080f024a22de249ce80133
Štěpán Stenchlák <s.stenchlak@gmail.com>
2021-10-04 09:25:01 +0200

    Fix registration

--- app/Modules/DefaultModule/RegistrationPresenter.php
+++ app/Modules/DefaultModule/RegistrationPresenter.php
@@ -10,8 +10,8 @@ class RegistrationPresenter extends BasePresenter
 {
     public static function isVisible(GamePhaseCalculator $gamePhaseCalculator): bool
     {
-        return $gamePhaseCalculator->isRegistration(GamePhaseCalculator::BEFORE) &&
-            $gamePhaseCalculator->isGame(GamePhaseCalculator::BEFORE);
+        return $gamePhaseCalculator->isRegistration(GamePhaseCalculator::NOW) ||
+            ($gamePhaseCalculator->isRegistration(GamePhaseCalculator::AFTER) && $gamePhaseCalculator->isGame(GamePhaseCalculator::BEFORE));
     }
 
     public function actionDefault()
