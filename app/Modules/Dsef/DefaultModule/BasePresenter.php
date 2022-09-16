ca6a7d77d16f6cf4d8eac869e2bf823ecc4ad590
vercah <vercaposta@seznam.cz>
2022-09-16 17:27:11 +0200

    details

--- app/Modules/Dsef/DefaultModule/BasePresenter.php
+++ app/Modules/Dsef/DefaultModule/BasePresenter.php
@@ -16,15 +16,15 @@ abstract class BasePresenter extends \App\Modules\Dsef\Core\BasePresenter
     protected function getNavItems(): array
     {
         $items = [];
-
-        $items[] = new NavItem(
-            new PageTitle(null, "Minulé ročníky", 'visible-sm-inline glyphicon glyphicon-info-sign'), // TODO
-            'Archive:',
-        );
         $items[] = new NavItem(
             new PageTitle(null, "Aktuální ročník", 'visible-sm-inline glyphicon glyphicon-info-sign'), // TODO
             'Current:',
         );
+        $items[] = new NavItem(
+            new PageTitle(null, "Minulé ročníky", 'visible-sm-inline glyphicon glyphicon-info-sign'), // TODO
+            'Archive:',
+        );
+
 
         return $items;
     }
