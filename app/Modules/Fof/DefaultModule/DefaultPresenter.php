6b04a639cd89bd2bff38531b5789c442a5e5fc8f
Štěpán Stenchlák <s.stenchlak@gmail.com>
2022-10-07 18:54:20 +0200

    countdown

--- app/Modules/Fof/DefaultModule/DefaultPresenter.php
+++ app/Modules/Fof/DefaultModule/DefaultPresenter.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Modules\Fof\DefaultModule;
 
+use App\Components\UpperHomeBeforeRegistration\UpperHomeBeforeRegistrationComponent;
 use App\Components\UpperHomePrague\UpperHomePrague;
 
 class DefaultPresenter extends BasePresenter
@@ -11,5 +12,10 @@ class DefaultPresenter extends BasePresenter
     protected function createComponentPrague(): UpperHomePrague
     {
         return new UpperHomePrague($this->getContext());
+    }
+
+    protected function createComponentUpperHomeBeforeRegistration(): UpperHomeBeforeRegistrationComponent
+    {
+        return new UpperHomeBeforeRegistrationComponent($this->gamePhaseCalculator);
     }
 }
