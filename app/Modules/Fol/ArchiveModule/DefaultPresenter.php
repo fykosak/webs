cdce2f0c0dc283ff253143cf3f6cbff3a38bf58b
Adam Krška <adam@krska.com>
2022-12-06 18:42:26 +0100

    fix archive map

--- app/Modules/Fol/ArchiveModule/DefaultPresenter.php
+++ app/Modules/Fol/ArchiveModule/DefaultPresenter.php
@@ -10,6 +10,6 @@ class DefaultPresenter extends BasePresenter
 {
     protected function createComponentMap(): MapComponent
     {
-        return new MapComponent($this->getContext(), $this->getEvent()->eventId);
+        return new MapComponent($this->getContext(), $this->gamePhaseCalculator);
     }
 }
