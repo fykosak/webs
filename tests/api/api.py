b8151fd7f5ea10b6735e2455532b24855c89c482
Adam Krška <adam@krska.com>
2023-07-26 19:39:01 +0200

    test with teams

--- tests/api/api.py
+++ tests/api/api.py
@@ -3,41 +3,6 @@ import os, glob
 
 app = Flask(__name__)
 
-def getDsefEvents():
-    dsefEvents = {}
-    index = 0
-    for filename in glob.glob(os.path.dirname(os.path.realpath(__file__)) + '/../../app/Modules/Dsef/ArchiveModule/templates/Default/simple*'):
-        year, month = os.path.basename(filename).rstrip('.latte').lstrip('simple.').split('-')
-        # generate event for every existing file
-        dsefEvents[str(index)] = {
-            "eventId":index,
-            "year":index,
-            "eventYear":index,
-            "begin":f"{year}-{month}-04T00:00:00+00:00",
-            "end":f"{year}-{month}-04T00:01:00+00:00",
-            "registrationBegin":"1970-01-01T00:00:00+00:00",
-            "registrationEnd":"1970-01-01T00:00:00+00:00",
-            "report": None,
-            "name":"Dsef",
-            "eventTypeId":2
-        }
-        index += 1
-    # add current event with ready registration
-    dsefEvents[str(index)] = {
-        "eventId":index,
-        "year":index,
-        "eventYear":index,
-        "begin":f"2099-12-04T00:00:00+00:00",
-        "end":f"2099-12-04T00:01:00+00:00",
-        "registrationBegin":"2099-12-04T00:01:00+00:00",
-        "registrationEnd":"2099-12-04T00:01:00+00:00",
-        "report": None,
-        "name":"Dsef",
-        "eventTypeId":2
-    }
-    return dsefEvents
-
-
 events = {
     "200": {
         "eventId":8,
@@ -65,6 +30,78 @@ events = {
     }
 }
 
+def getDsefEvents():
+    dsefEvents = {}
+    index = 0
+    for filename in glob.glob(os.path.dirname(os.path.realpath(__file__)) + '/../../app/Modules/Dsef/ArchiveModule/templates/Default/simple*'):
+        year, month = os.path.basename(filename).rstrip('.latte').lstrip('simple.').split('-')
+        # generate event for every existing file
+        dsefEvents[str(index)] = {
+            "eventId":index,
+            "year":index,
+            "eventYear":index,
+            "begin":f"{year}-{month}-04T00:00:00+00:00",
+            "end":f"{year}-{month}-04T00:01:00+00:00",
+            "registrationBegin":"1970-01-01T00:00:00+00:00",
+            "registrationEnd":"1970-01-01T00:00:00+00:00",
+            "report": None,
+            "name":"Dsef",
+            "eventTypeId":2
+        }
+        index += 1
+    # add current event with ready registration
+    dsefEvents[str(index)] = {
+        "eventId":index,
+        "year":index,
+        "eventYear":index,
+        "begin":"2099-12-04T00:00:00+00:00",
+        "end":"2099-12-04T00:01:00+00:00",
+        "registrationBegin":"2099-12-04T00:01:00+00:00",
+        "registrationEnd":"2099-12-04T00:01:00+00:00",
+        "report": None,
+        "name":"Dsef",
+        "eventTypeId":2
+    }
+    return dsefEvents
+
+def getTeams():
+    states = ["participated", "disqualified", "applied", "pending", "approved", "canceled"]
+    memberCountries = ["cs", "sk", "zz", None]
+    teams = []
+    teamIndex = 0
+    for state in states:
+        team = {
+            "teamId":7047,
+            "name":f"Team {teamIndex}",
+            "status":state,
+            "category":"A",
+            "created":"1970-01-01T00:00:00+00:00",
+            "phone":"+42012345679",
+            "password":None,
+            "points":24,
+            "rankCategory":70,
+            "rankTotal":224,
+            "forceA":0,
+            "gameLang":"cs",
+            "members":[],
+            "teachers":[]
+        }
+        teamIndex += 1
+        memberIndex = 0
+        for country in memberCountries:
+            team["members"].append({
+                "name":f"Test {memberIndex}",
+                "personId":teamIndex * 5 + memberIndex,
+                "email":"test@example.com",
+                "schoolId":1,
+                "schoolName":"MFF UK",
+                "studyYear":1,
+                "countryIso":country
+            })
+            memberIndex += 1
+        teams.append(team)
+    return teams
+
 @app.get("/GetEventList")
 def getEventList():
     eventTypeId = request.args.get('event_type_ids[0]', default=None)
@@ -81,8 +118,12 @@ def getEvent():
         return "Event id not specified", 400
     dsefEvents = getDsefEvents()
     if (eventId in dsefEvents):
+        event = dsefEvents[eventId]
+        event["teams"] = getTeams()
         return dsefEvents[eventId]
     if (eventId in events):
+        event = events[eventId]
+        event["teams"] = getTeams()
         return events[eventId]
     return "Invalid event id", 400
 
