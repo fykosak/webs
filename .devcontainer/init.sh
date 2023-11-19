5e4ab0e1888943f07805fece783e027e137a6435
Pavel Provazník <41260648+paproc@users.noreply.github.com>
2023-11-20 00:45:49 +0100

    set to .local

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -5,11 +5,21 @@ composer install
 npm install
 #npm run build
 
-cp app/config/config.local.neon.example app/config/config.fof.local.neon
-cp app/config/config.local.neon.example app/config/config.fol.local.neon
-cp app/config/config.local.neon.example app/config/config.dsef.local.neon
-cp app/config/config.local.neon.example app/config/config.vyfuk.local.neon
-cp app/config/config.local.neon.example app/config/config.fykos.local.neon
+if [ ! -f app/config/config.fof.local.neon ]; then
+cp .devcontainer/config.local.neon.example app/config/config.fof.local.neon
+fi
+if [ ! -f app/config/config.fol.local.neon ]; then
+cp .devcontainer/config.local.neon.example app/config/config.fol.local.neon
+fi
+if [ ! -f app/config/config.dsef.local.neon ]; then
+cp .devcontainer/config.local.neon.example app/config/config.dsef.local.neon
+fi
+if [ ! -f app/config/config.vyfuk.local.neon ]; then
+cp .devcontainer/config.local.neon.example app/config/config.vyfuk.local.neon
+fi
+if [ ! -f app/config/config.fykos.local.neon ]; then
+cp .devcontainer/config.local.neon.example app/config/config.fykos.local.neon
+fi
 
 app/i18n/compile.sh
 rm -rf temp/cache
