4d2fa405c94115a929f5049c54747f2eeaaf406d
vanekm <martin.vanek@fykos.cz>
2023-11-20 19:30:58 +0100

    attempt to fix formatting

--- app/Components/TeamResults/TeamResultsComponent.php
+++ app/Components/TeamResults/TeamResultsComponent.php
@@ -120,7 +120,7 @@ class TeamResultsComponent extends BaseComponent
 
         return false;
     }
-    
+
     /**
      * @throws \Throwable
      */
@@ -153,26 +153,26 @@ class TeamResultsComponent extends BaseComponent
 
         // countries
         arsort($countryISOs);
-        
+
         $countryISOContainer = $form->addContainer('country_iso');
         foreach ($countryISOs as $countryISO => $count) {
             $countryISOContainer->addCheckbox($countryISO, $countryISO)
                 ->setOption('count', $count);
-        }        
+        }
 
-        
+
         $form->addButton('reset')->setHtmlAttribute('type', 'reset')->setHtmlAttribute('class', 'btn btn-dark');
 
         $form->addSubmit('applyFilters', 'Apply')->setHtmlAttribute('class', 'btn btn-primary');
 
-        $form->onSuccess[] = fn(Form $form) => $this->filterData = $form->getValues('array');
+        $form->onSuccess[] = fn (Form $form) => $this->filterData = $form->getValues('array');
         // $form->onSuccess[] = function(Form $form) {
         //     $this->filterData = $form->getValues('array');
         //     $this->redirect('this', ['filterData' => $this->filterData]);
         // };
 
         // $form->setRenderer(new \App\Renderers\CustomFormRenderer($this->lang));
-        
+
         return $form;
     }
 }
