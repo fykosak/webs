9d687364c228f8c10b9f9d5f15ee1028cae8f673
vercah <vercaposta@seznam.cz>
2023-11-25 22:56:07 +0100

    add hall of fame

--- app/Modules/Fykos/DefaultModule/AboutPresenter.php
+++ app/Modules/Fykos/DefaultModule/AboutPresenter.php
@@ -17,12 +17,11 @@ final class AboutPresenter extends BasePresenter
         $this->downloader = $downloader;
     }
 
-    public function renderOrganizers(): void
+    public function parseOrganizers(): array
     {
-        $response = $this->downloader->download(new OrganizersRequest(1, 37));
+        $response = $this->downloader->download(new OrganizersRequest(1));
         $organizers = json_decode($response, true);
 
-        // parse orgs
         if ($organizers !== null) {
             $parsedOrganizers = [];
             foreach ($organizers as $organizer) {
@@ -41,17 +40,32 @@ final class AboutPresenter extends BasePresenter
                     'texSignature' => $organizer['texSignature'],
                     'domainAlias' => $organizer['domainAlias'],
                 ];
-                
-                // only add current organizers
-                $current_year = 37;
-                if ($parsedOrganizer['until'] == null or $parsedOrganizer['until'] == $current_year) {
-                    $parsedOrganizers[] = $parsedOrganizer;    
-                }
-                
+                $parsedOrganizers[] = $parsedOrganizer;
             }
+            return $parsedOrganizers;
+        }
+        return [];
+    }
+
+    public function isCurrentOrganizer(array $organizer): bool
+    {
+        $currentYear = 37;
+        if ($organizer['until'] == null or $organizer['until'] == $currentYear) {
+            return true;
+        }
+        return false;
+    }
+
+    public function renderOrganizers(): void
+    {
+        $allOrganizers = $this->parseOrganizers();
+        $currentOrganizers = [];
+
+        if ($allOrganizers !== []) {
+            $currentOrganizers = array_filter($allOrganizers, array($this, "isCurrentOrganizer"));
 
             // sort by order
-            usort($parsedOrganizers, function ($a, $b) {
+            usort($currentOrganizers, function ($a, $b) {
                 if ($a['order'] === $b['order']) {
                     $last_name_a = explode(' ', $a['name']);
                     $last_name_a = end($last_name_a);
@@ -61,8 +75,27 @@ final class AboutPresenter extends BasePresenter
                 }
                 return $b['order'] <=> $a['order'];
             });
-
-            $this->template->organizers = $parsedOrganizers;
         }
+            $this->template->currentOrganizers = $currentOrganizers;
     }
+
+    public function renderFame(): void {
+        $allOrganizers = $this->parseOrganizers();
+
+        if ($allOrganizers !== []) {
+            // sort by order
+            usort($allOrganizers, function ($a, $b) {
+                if ($a['since'] === $b['since']) {
+                    $last_name_a = explode(' ', $a['name']);
+                    $last_name_a = end($last_name_a);
+                    $last_name_b = explode(' ', $b['name']);
+                    $last_name_b = end($last_name_b);
+                    return $last_name_a <=> $last_name_b;
+                }
+                return $a['since'] <=> $b['since'];
+            });
+        }
+        $this->template->allOrganizers = $allOrganizers;
+    }
+
 }
