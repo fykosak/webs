3422aaca8ab57172dacabbfb61f4141d5d3cb074
vercah <vercaposta@seznam.cz>
2023-11-25 21:33:24 +0100

    cleanup in base presenters

--- app/Modules/Fykos/DefaultModule/BasePresenter.php
+++ app/Modules/Fykos/DefaultModule/BasePresenter.php
@@ -4,10 +4,6 @@ declare(strict_types=1);
 
 namespace App\Modules\Fykos\DefaultModule;
 
-use App\Modules\Dsef\DefaultModule\CurrentPresenter;
-use App\Modules\Dsef\DefaultModule\RegistrationPresenter;
-use Fykosak\Utils\UI\Navigation\NavItem;
-use Fykosak\Utils\UI\PageTitle;
 
 abstract class BasePresenter extends \App\Modules\Fykos\Core\BasePresenter
 {
--- app/Modules/Fykos/EventsModule/BasePresenter.php
+++ app/Modules/Fykos/EventsModule/BasePresenter.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Modules\Fykos\EventsModule;
 
-use Fykosak\Utils\UI\Navigation\NavItem;
-use Fykosak\Utils\UI\PageTitle;
 
 abstract class BasePresenter extends \App\Modules\Fykos\Core\BasePresenter
 {
