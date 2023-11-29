87385d676c66db50dfb315a99a593b4385ec4c0e
Adam Krška <adam@krska.com>
2023-11-29 11:12:53 +0100

    fix points for cancelled tasks

--- app/Components/ApiResults/use-team-points.ts
+++ app/Components/ApiResults/use-team-points.ts
@@ -5,9 +5,13 @@ import {Simulate} from "react-dom/test-utils";
 export const useTeamPoints = (data: DataInterface<true> | null): {team: Team<true>, points: number, lastSubmit: Date}[] | null => {
   return useMemo(() => {
     if (data) {
+      let tasks : number[] = [];
+      data.tasks.forEach(task => {
+        tasks.push(task.taskId);
+      })
       return data.teams.map(team => {
         let submits = Object.values(data.submits);
-        const teamSubmits = submits.filter(submit => submit.teamId === team.teamId);
+        const teamSubmits = submits.filter(submit => submit.teamId === team.teamId && tasks.includes(submit.taskId));
         return {
           team,
           points: teamSubmits.reduce((prev, curr) => prev + (curr.skipped ? -1 : curr.points), 0)
