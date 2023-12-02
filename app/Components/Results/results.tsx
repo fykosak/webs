be361b451c07628bb7a15a386906716c5a01ab18
vanekm <martin.vanek@fykos.cz>
2023-12-02 21:07:01 +0100

    random intermediate progress on results -- added filtering, borders

--- app/Components/Results/results.tsx
+++ app/Components/Results/results.tsx
@@ -36,8 +36,40 @@ interface Props<Category extends string = string> {
     };
 }
 
-function CategoryResults({submits, tasks}: { submits: Submits, tasks: Tasks }) {
+function CategoryResults({ submits, tasks }: { submits: Submits, tasks: Tasks }) {
     const [activeSeries, setActiveSeries] = useState<{ [key: string]: boolean }>({});
+    const [sortColumn, setSortColumn] = useState<string | null>(null);
+    const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>('asc');
+
+    const toggleSort = (column: string) => {
+        if (sortColumn === column) {
+            setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc');
+        } else {
+            setSortColumn(column);
+            setSortDirection('asc');
+        }
+    };
+
+    const sortedSubmits = React.useMemo(() => {
+        if (sortColumn) {
+            const sorted = [...submits];
+            sorted.sort((a, b) => {
+                if (sortColumn === 'Name') {
+                    const lastNameA = a.contestant.name.split(' ').pop();
+                    const lastNameB = b.contestant.name.split(' ').pop();
+                    return sortDirection === 'asc' ? lastNameA.localeCompare(lastNameB) : lastNameB.localeCompare(lastNameA);
+                } else if (sortColumn === 'School') {
+                    return sortDirection === 'asc' ? a.contestant.school.localeCompare(b.contestant.school) : b.contestant.school.localeCompare(a.contestant.school);
+                } else if (sortColumn === 'Rank') {
+                    return sortDirection === 'asc' ? a.rank[0] - b.rank[0] : b.rank[0] - a.rank[0];
+                }
+                return 0;
+            });
+            return sorted;
+        }
+        return submits;
+    }, [submits, sortColumn, sortDirection]);
+
     const head = [];
     for (const series in tasks) {
         const tasksInSeries = tasks[series];
@@ -48,126 +80,180 @@ function CategoryResults({submits, tasks}: { submits: Submits, tasks: Tasks }) {
             });
             head.push(<>{subTasks}</>);
         }
-        head.push(<th onClick={() => {
-            const newActiveSeries = { [series]: !active };
-            for (const s in activeSeries) {
-                if (s !== series) {
-                    newActiveSeries[s] = false;
-                }
-            }
-            setActiveSeries(newActiveSeries);
-        }}>Series {series}</th>);
-
+        head.push(
+            <th
+                onClick={() => {
+                    const newActiveSeries = { [series]: !active };
+                    for (const s in activeSeries) {
+                        if (s !== series) {
+                            newActiveSeries[s] = false;
+                        }
+                    }
+                    setActiveSeries(newActiveSeries);
+                }}
+                className="centered-cell"
+            >
+                {series}
+            </th>
+        );
     }
-    return <table className="table table-hover contest-results table-sm">
-        <thead>
-        <tr>
-            <th>Name</th>
-            <th>School</th>
-            {head}
-        </tr>
-        </thead>
-        <tbody>{submits.map((contestant) => {
-            const seriesContainers = [];
-            for (const series in tasks) {
-                const tasksInSeries = tasks[series];
-                seriesContainers.push(<SeriesResults
-                    series={series}
-                    key={series}
-                    tasks={tasksInSeries}
-                    contestant={contestant}
-                    show={activeSeries[series] || false}
-                />);
-            }
-            return <tr>
-                <td>{contestant.contestant.name}</td>
-                <td>{contestant.contestant.school}</td>
-                {seriesContainers}
-            </tr>;
-        })}</tbody>
-    </table>;
+
+    return (
+        <table className="table table-hover contest-results table-sm">
+            <thead>
+                <tr>
+                    <th rowSpan={2} className="centered-cell">
+                        <a onClick={() => toggleSort('Rank')}>
+                            Rank
+                            {sortColumn === 'Rank' && (
+                                <span style={{ color: 'black' }}>
+                                    {sortDirection === 'asc' ?  '↓' : '↑'}
+                                </span>
+                            )}
+                        </a>
+                    </th>
+                    <th rowSpan={2}>
+                        <a onClick={() => toggleSort('Name')}>
+                            Name
+                            {sortColumn === 'Name' && (
+                                <span style={{ color: 'black' }}>
+                                    {sortDirection === 'asc' ? '↓' : '↑'}
+                                </span>
+                            )}
+                        </a>
+                    </th>
+                    <th rowSpan={2}>
+                        <a onClick={() => toggleSort('School')}>
+                            School
+                            {sortColumn === 'School' && (
+                                <span style={{ color: 'black' }}>
+                                    {sortDirection === 'asc' ? '↓' : '↑'}
+                                </span>
+                            )}
+                        </a>
+                    </th>
+                    <th colSpan={Object.keys(tasks).length}>Series</th>
+                </tr>
+                <tr>{head}</tr>
+            </thead>
+            <tbody>
+                {sortedSubmits.map((contestant) => {
+                    const seriesContainers = [];
+                    for (const series in tasks) {
+                        const tasksInSeries = tasks[series];
+                        seriesContainers.push(
+                            <SeriesResults
+                                series={series}
+                                key={series}
+                                tasks={tasksInSeries}
+                                contestant={contestant}
+                                show={activeSeries[series] || false}
+                            />
+                        );
+                    }
+                    return (
+                        <tr>
+                            <td className="centered-cell">{contestant.rank[0]}</td>
+                            <td>{contestant.contestant.name}</td>
+                            <td>{contestant.contestant.school}</td>
+                            {seriesContainers}
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    );
 }
 
 interface SeriesResultsProps {
-    tasks: Array<Task>,
-    contestant: Contestant,
-    show: boolean,
-    series: string
+    tasks: Array<Task>;
+    contestant: Contestant;
+    show: boolean;
+    series: string;
 }
 
-function SeriesResults(
-    {
-        tasks,
-        contestant,
-        show,
-        series,
-    }: SeriesResultsProps,
-) {
+function SeriesResults({
+    tasks,
+    contestant,
+    show,
+    series,
+}: SeriesResultsProps) {
     let sum = 0;
-    const subTasks = tasks.map((task) => {
+    const subTasks = tasks.map((task, index) => {
         let badge;
         if (contestant.submits.hasOwnProperty(task.taskId)) {
             if (contestant.submits[task.taskId] === null) {
                 badge = <span className="points points-na">?</span>;
             } else {
                 sum += contestant.submits[task.taskId];
-                badge =
-                    <span className="points points-ok">{contestant.submits[task.taskId]}</span>;
+                badge = <span className="points points-ok">{contestant.submits[task.taskId]}</span>;
             }
         } else {
             badge = <span className="points points-no">-</span>;
         }
-        return <td data-series={series} key={task.label} data-label={task.label}>{badge}</td>
+        const isFirstSeries = index === 0;
+        const isLastSeries = index === tasks.length - 1;
+        const classNames = `centered-cell ${isFirstSeries ? 'border-left' : ''} ${
+            isLastSeries ? 'border-right' : ''
+        }`;
+        return (
+            <td data-series={series} key={task.label} data-label={task.label} className={classNames}>
+                {badge}
+            </td>
+        );
     });
-    return <>{show && subTasks}
-        <td data-series={series}><strong>{sum}</strong></td>
-    </>;
+    return (
+        <>
+            {show && subTasks}
+            <td data-series={series} className="centered-cell border-left border">
+                <strong>{sum}</strong>
+            </td>
+        </>
+    );
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     const element = document.getElementById('contest-results');
     if (element) {
         const data = JSON.parse(element.getAttribute('data-data'));
-        ReactDOM.render(<Results resultsData={data}/>, element);
+        ReactDOM.render(<Results resultsData={data} />, element);
     }
 });
-
-
 
 function Results({ resultsData }: Props) {
     const [activeCategories, setActiveCategories] = useState<{ [key: string]: boolean }>({});
 
-        const toggleCategory = (category: string) => {
-            setActiveCategories((prevActiveCategories) => ({
-                ...prevActiveCategories,
-                [category]: !prevActiveCategories[category],
-            }));
-        };
+    const toggleCategory = (category: string) => {
+        setActiveCategories((prevActiveCategories) => ({
+            ...prevActiveCategories,
+            [category]: !prevActiveCategories[category],
+        }));
+    };
 
-        const categoryContainers = Object.keys(resultsData.submits).map((category) => {
-            const categoryNumber = category.slice(-1);
-            return (
-                <div>
-                    <button
-                        className="btn btn-primary button-collapse-header"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target={`#collapse-category-${category}`}
-                        aria-expanded={activeCategories[category] ? "true" : "false"}
-                        aria-controls={`collapse-category-${category}`}
-                        onClick={() => toggleCategory(category)}
-                    >
-                        Kategorie {categoryNumber}. ročníků
-                    </button>
-                    <div
-                        className={`collapse toggle-content ${activeCategories[category] ? "show" : ""}`}
-                        id={`collapse-category-${category}`}
-                    >
-                        <CategoryResults submits={resultsData.submits[category]} tasks={resultsData.tasks[category]} />
-                    </div>
+    const categoryContainers = Object.keys(resultsData.submits).map((category) => {
+        const categoryNumber = category.slice(-1);
+        return (
+            <div>
+                <button
+                    className="btn btn-primary button-collapse-header"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target={`#collapse-category-${category}`}
+                    aria-expanded={activeCategories[category] ? 'true' : 'false'}
+                    aria-controls={`collapse-category-${category}`}
+                    onClick={() => toggleCategory(category)}
+                >
+                    Kategorie {categoryNumber}. ročníků
+                </button>
+                <div
+                    className={`collapse toggle-content ${activeCategories[category] ? 'show' : ''}`}
+                    id={`collapse-category-${category}`}
+                >
+                    <CategoryResults submits={resultsData.submits[category]} tasks={resultsData.tasks[category]} />
                 </div>
-            );
-        });
+            </div>
+        );
+    });
 
-        return <div>{categoryContainers}</div>;
-    }
+    return <div>{categoryContainers}</div>;
+}
