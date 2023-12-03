32f014d7da657d4e01d2f14248b7b6265967eb26
Vojtěch Kubrycht <vojtakubrycht@seznam.cz>
2023-12-03 21:50:05 +0000

    add navbar links, presenters

--- app/Modules/Vyfuk/Core/BasePresenter.php
+++ app/Modules/Vyfuk/Core/BasePresenter.php
@@ -11,17 +11,52 @@ abstract class BasePresenter extends \App\Modules\Core\BasePresenter
 {
     protected function getNavItems(): array
     {
-        return [
-            new NavItem(new PageTitle(null, 'O Výfuku'), ':default'),
-            new NavItem(new PageTitle(null, 'Zadání', 'fas fa-pen'), ':default'),
-            new NavItem(new PageTitle(null, 'Pořadí'), ':default'),
-            new NavItem(new PageTitle(null, 'Akce'), ':default', [], [
-                new NavItem(new PageTitle(null, 'Tábor'), ':default'),
-                new NavItem(new PageTitle(null, 'Setkání'), ':default'),
-                new NavItem(new PageTitle(null, 'Ostatní'), ':default')
-            ]),
-            new NavItem(new PageTitle(null, 'Login'), 'https://db.fykos.cz')
-        ];
+        $items = [];
+
+        $items[] = new NavItem(
+            new PageTitle(null, 'O Nás'),
+            ':default',
+            [],
+            [
+                new NavItem(new PageTitle(null, 'Co je Výfuk?'), ':Default:About:default'),
+                new NavItem(new PageTitle(null, 'Historie'), ':Default:About:History'),
+                new NavItem(new PageTitle(null, 'Organizátoři'), ':Default:About:Organizers'),
+                new NavItem(new PageTitle(null, 'Sponzoři'), ':Default:About:Sponsors'),
+                new NavItem(new PageTitle(null, 'Kontakt'), ':Default:About:Contact'),
+            ],
+        );
+
+        $items[] = new NavItem(
+            new PageTitle(null, 'Zadání', 'fas fa-pen'),
+            ':Default:Problems:default'
+        );
+
+        $items[] = new NavItem(
+            new PageTitle(null, 'Pořadí'),
+            ':Default:Results:default'
+        );
+
+        $items[] = new NavItem(
+            new PageTitle(null, 'Akce'),
+            ':default',
+            [],
+            [
+                new NavItem(new PageTitle(null, 'Tábor'), ':default'), //TODO
+                new NavItem(new PageTitle(null, 'Setkání'), ':default'), //TODO
+            ],
+        );
+
+        $items[] = new NavItem(
+            new PageTitle(null, 'Archiv úloh'),
+            ':Default:Archive:default'
+        );
+
+        $items[] = new NavItem(
+            new PageTitle(null, 'Přihlásit se'),
+            'https://db.fykos.cz'
+        );
+
+        return $items;
     }
 
     protected function localize(): void
