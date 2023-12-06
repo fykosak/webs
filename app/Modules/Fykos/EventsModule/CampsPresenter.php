f4cbd38f01a0a8a622b4d3b7cc4ef0d035d32ab3
vanekm <martin.vanek@fykos.cz>
2023-12-06 13:36:46 +0100

    first attempt at embedding camps

---
+++ app/Modules/Fykos/EventsModule/CampsPresenter.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+
+
+namespace App\Modules\Fykos\EventsModule;
+
+use DOMDocument;
+
+
+class CampsPresenter extends BasePresenter
+{
+
+
+    public function renderDefault()
+    {
+
+        // Fetch the webpage
+        $html = file_get_contents('https://fykos.cz/akce/soustredeni/start');
+
+
+        // Create a new DOM document
+        $doc = new DOMDocument;
+
+        // Suppress errors due to ill-formed HTML
+        libxml_use_internal_errors(true);
+
+        // Load the HTML
+        $doc->loadHTML($html);
+
+        // Clear the errors
+        libxml_clear_errors();
+
+        // Extract the required part
+        $content = $doc->getElementById('dokuwiki__content');
+
+        // Display the extracted part
+        $doc->saveHTML($content);
+
+        $this->template->oldContent = $content;
+    }
+}
