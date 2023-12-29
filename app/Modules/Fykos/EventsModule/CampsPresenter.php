7908a69f988c3dc0ad672dc8f0d8d668726aed32
vanekm <martin.vanek@fykos.cz>
2023-12-29 16:55:56 +0100

    camps generic pages

--- app/Modules/Fykos/EventsModule/CampsPresenter.php
+++ app/Modules/Fykos/EventsModule/CampsPresenter.php
@@ -8,29 +8,53 @@ use DOMDocument;
 
 class CampsPresenter extends BasePresenter
 {
+
     public function renderDefault()
     {
-        // Fetch the webpage
-        $html = file_get_contents('https://fykos.cz/akce/soustredeni/start');
+        // // Fetch the webpage
+        // $html = file_get_contents('https://fykos.cz/akce/soustredeni/start');
 
-        // Create a new DOM document
-        $doc = new DOMDocument();
+        // // Create a new DOM document
+        // $doc = new DOMDocument();
 
-        // Suppress errors due to ill-formed HTML
-        libxml_use_internal_errors(true);
+        // // Suppress errors due to ill-formed HTML
+        // libxml_use_internal_errors(true);
 
-        // Load the HTML
-        $doc->loadHTML($html);
+        // // Load the HTML
+        // $doc->loadHTML($html);
 
-        // Clear the errors
-        libxml_clear_errors();
+        // // Clear the errors
+        // libxml_clear_errors();
 
-        // Extract the required part
-        $content = $doc->getElementById('dokuwiki__content');
+        // // Extract the required part
+        // $content = $doc->getElementById('dokuwiki__content');
 
-        // Display the extracted part
-        $htmlContent = $doc->saveHTML($content);
+        // // Display the extracted part
+        // $htmlContent = $doc->saveHTML($content);
 
-        $this->template->oldContent = $htmlContent;
+        // $this->template->oldContent = $htmlContent;
+
+        // TODO: get this from db
+        $this->template->camps = [
+            [
+                "title" => "název",
+                "description" => "I'm sorry, but as an AI programming assistant, I don't have the capability to generate random text like Lorem Ipsum. However, you can easily generate Lorem Ipsum text using online tools or libraries in PHP.",
+                "photo_path" => "/images/events/internships/staze1.jpg",
+                "year" => "37",
+                "season" => "podzim"
+            ],
+            [
+                "title" => "název2",
+                "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "photo_path" => "/images/events/internships/staze2.jpg",
+                "year" => "38",
+                "season" => "jaro"
+            ]
+        ];
+
+        foreach ($this->template->camps as &$camp) {
+            $camp['link'] = 'https://fykos.cz/rocnik' . $camp['year'] . '/sous-' . $camp['season'] . '/start';
+        }
+
     }
 }
