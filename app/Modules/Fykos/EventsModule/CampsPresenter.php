36ade47d38b299bde56c9f899722c38ae4481d82
vanekm <martin.vanek@fykos.cz>
2024-01-14 21:22:55 +0100

    sous graphics

--- app/Modules/Fykos/EventsModule/CampsPresenter.php
+++ app/Modules/Fykos/EventsModule/CampsPresenter.php
@@ -9,6 +9,7 @@ use Fykosak\FKSDBDownloaderCore\Requests\EventListRequest;
 use Fykosak\FKSDBDownloaderCore\Requests\EventRequest;
 use Fykosak\FKSDBDownloaderCore\Requests\ParticipantsRequest;
 use Nette\Application\ForbiddenRequestException;
+// use Tracy\Debugger;
 
 class CampsPresenter extends BasePresenter
 {
@@ -42,50 +43,18 @@ class CampsPresenter extends BasePresenter
     public function renderDefault(): void
     {
         $data = $this->downloader->download('fksdb', new EventListRequest(self::CAMPS_IDS));
+        
+        // sort by date
+        usort($data, function($a, $b) {
+            return strtotime($b['begin']) - strtotime($a['begin']);
+        });
+
         $this->template->data = $data;
-        // // Fetch the webpage
-        // $html = file_get_contents('https://fykos.cz/akce/soustredeni/start');
+        
+        // Debugger::dump($data); // Display data in Nette Debugger
 
-        // // Create a new DOM document
-        // $doc = new DOMDocument();
-
-        // // Suppress errors due to ill-formed HTML
-        // libxml_use_internal_errors(true);
-
-        // // Load the HTML
-        // $doc->loadHTML($html);
-
-        // // Clear the errors
-        // libxml_clear_errors();
-
-        // // Extract the required part
-        // $content = $doc->getElementById('dokuwiki__content');
-
-        // // Display the extracted part
-        // $htmlContent = $doc->saveHTML($content);
-
-        // $this->template->oldContent = $htmlContent;
-
-        // TODO: get this from db
-        $this->template->camps = [
-            [
-                "title" => "název",
-                "description" => "I'm sorry, but as an AI programming assistant, I don't have the capability to generate random text like Lorem Ipsum. However, you can easily generate Lorem Ipsum text using online tools or libraries in PHP.",
-                "photo_path" => "/images/events/internships/staze1.jpg",
-                "year" => "37",
-                "season" => "podzim"
-            ],
-            [
-                "title" => "název2",
-                "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                "photo_path" => "/images/events/internships/staze2.jpg",
-                "year" => "38",
-                "season" => "jaro"
-            ]
-        ];
-
-        foreach ($this->template->camps as &$camp) {
-            $camp['link'] = 'https://fykos.cz/rocnik' . $camp['year'] . '/sous-' . $camp['season'] . '/start';
-        }
+        // foreach ($this->template->camps as &$camp) {
+        //     $camp['link'] = 'https://fykos.cz/rocnik' . $camp['year'] . '/sous-' . $camp['season'] . '/start';
+        // }
     }
 }
