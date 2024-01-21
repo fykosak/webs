5a5a6e2d3733edbe49c544d1d4e03b2aef8a276e
vanekm <martin.vanek@fykos.cz>
2024-01-21 21:24:02 +0100

    init persons and years

---
+++ app/Components/Person/PersonComponent.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Components\Person;
+
+use Fykosak\Utils\BaseComponent\BaseComponent;
+use App\Models\Downloader\FKSDBDownloader;
+use Fykosak\FKSDBDownloaderCore\Requests\OrganizersRequest;
+
+use Nette\DI\Container;
+
+class PersonComponent extends BaseComponent
+{
+    private FKSDBDownloader $downloader;
+
+    private array $organizers;
+
+    public function __construct(Container $container)
+    {
+        parent::__construct($container);
+        $this->downloader = $container->getByType(FKSDBDownloader::class);
+
+        $this->organizers = $this->parseOrganizers();
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    public function parseOrganizers(): array
+    {
+        $organizers = $this->downloader->download(new OrganizersRequest(1));
+
+        $parsedOrganizers = [];
+        foreach ($organizers as $organizer) {
+            $parsedOrganizer = [
+                'name' => $organizer['name'],
+                'personId' => $organizer['personId'],
+                'email' => $organizer['email'],
+                'academicDegreePrefix' => $organizer['academicDegreePrefix'],
+                'academicDegreeSuffix' => $organizer['academicDegreeSuffix'],
+                'career' => $organizer['career'],
+                'contribution' => $organizer['contribution'],
+                'order' => $organizer['order'],
+                'role' => $organizer['role'],
+                'since' => $organizer['since'],
+                'until' => $organizer['until'],
+                'texSignature' => $organizer['texSignature'],
+                'domainAlias' => $organizer['domainAlias'],
+            ];
+            $parsedOrganizers[] = $parsedOrganizer;
+        }
+        return $parsedOrganizers;
+    }
+
+    public function get_org(array $organizers, $id): array
+    {
+        foreach ($organizers as $organizer) {
+            if ($organizer['personId'] == $id) {
+                return $organizer;
+            }
+        }
+        return [];
+    }
+
+    public function render($id = null)
+    {
+        $this->template->person = $this->get_org($this->organizers, $id);
+        $this->template->render(__DIR__ . DIRECTORY_SEPARATOR . 'person.latte');
+    }
+}
---
+++ app/Modules/Fykos/DefaultModule/YearPresenter.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Fykos\DefaultModule;
+
+use App\Components\Person\PersonComponent;
+
+class YearPresenter extends BasePresenter
+{
+    public $year = 36; // todo get from url
+
+    // read from JSON
+    public function getYearData(int $year): array
+    {
+        $json = file_get_contents(__DIR__ . "/templates/Year/YearData/$year.json");
+        $data = json_decode($json, true);
+
+        $data = $this->parseYearData($data);
+
+        return $data;
+    }
+
+    public function parseYearData(array $data): array
+    {
+        foreach ($data['events'] as &$event) {
+            if (!isset($event['name'])) {
+                $event['name'] = $event['type'];
+                switch ($event['type']) {
+                    case 'fof':
+                        $event['name'] = 'Fyziklání';
+                        break;
+                    case 'naboj':
+                        $event['name'] = 'Fyzikální Náboj';
+                        break;
+                    case 'fol':
+                        $event['name'] = 'Fyziklání Online';
+                        break;
+                    case 'sous-jaro':
+                        $event['name'] = "Jarní soustředění {$this->year}. ročníku";
+                        break;
+                    case 'sous-podzim':
+                        $event['name'] = "Podzimní soustředění {$this->year}. ročníku";
+                        break;
+                    case 'dsef':
+                        $event['name'] = 'Den s experimentální fyzikou';
+                        break;
+                }
+            };
+            $event['img-path'] = "/images/events/default/fykos.png";
+            $event["n-href-url"] = "/images/events/default/fykos.png";
+        }
+        return $data;
+    }
+
+    public function createComponentPerson(): PersonComponent
+    {
+        return new PersonComponent($this->getContext());
+    }
+
+    public function renderDefault(): void
+    {
+        $this->template->data = $this->getYearData($this->year);
+        $this->template->year = $this->year;
+    }
+}
