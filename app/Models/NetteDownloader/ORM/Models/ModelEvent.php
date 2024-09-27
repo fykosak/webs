d90dcfc4c47c22f83775459e04dba59dc09c5463
Pavel Provazník <41260648+paproc@users.noreply.github.com>
2024-09-27 10:55:04 +0200

    html events

--- app/Models/NetteDownloader/ORM/Models/ModelEvent.php
+++ app/Models/NetteDownloader/ORM/Models/ModelEvent.php
@@ -8,34 +8,35 @@ use Fykosak\Utils\DateTime\Period;
 
 class ModelEvent
 {
-    public int $eventId;
-    public int $eventTypeId;
-    public string $name;
-    public int $eventYear;
-    public int $year;
-    public \DateTimeImmutable $begin;
-    public \DateTimeImmutable $end;
-    public \DateTimeImmutable $registrationBegin;
-    public \DateTimeImmutable $registrationEnd;
-    public ?string $report;
+    public readonly int $eventId;
+    public readonly int $eventTypeId;
+    public readonly string $name;
+    public readonly int $eventYear;
+    public readonly int $year;
+    public readonly \DateTimeImmutable $begin;
+    public readonly \DateTimeImmutable $end;
+    public readonly \DateTimeImmutable $registrationBegin;
+    public readonly \DateTimeImmutable $registrationEnd;
+    public readonly ?string $report;
     /**
      * @var string[] $reportNew
      */
-    public array $reportNew;
+    public readonly array $reportNew;
     /**
      * @var string[] $description
      */
-    public array $description;
+    public readonly array $description;
     /**
      * @var string[] $nameNew
      */
-    public array $nameNew;
-    public ?string $place;
-    public ?int $contestId;
-    public ?array $schedule;
+    public readonly array $nameNew;
+    public readonly ?string $place;
+    public readonly ?int $contestId;
+    public readonly ?array $schedule;
 
-    public ModelGame|null $game;
-    public Period $registration;
+
+    public readonly ModelGame|null $game;
+    public readonly Period $registration; // TODO magic?
 
     public function getRegistrationPeriod(): Period
     {
@@ -58,7 +59,6 @@ class ModelEvent
         $end = $this->begin->add(new \DateInterval('P1D'));
         return new Period($begin, $end);
     }
-
     /**
      * Returns true about a week after the event when no one is interested in game already.
      * @throws \Throwable
@@ -71,40 +71,11 @@ class ModelEvent
 
     public function getYear(): int
     {
-        return (int) $this->begin->format('Y');
+        return (int)$this->begin->format('Y');
     }
 
     public function getMonth(): string
     {
         return $this->begin->format('m');
-    }
-
-    public function makeParagraphs(string $attr): ModelEvent
-    {
-        if (is_string($this->{$attr})) {
-            $this->{$attr} = $this->makeOneParagraphs($this->{$attr});
-        } elseif (is_array($this->{$attr})) {
-            foreach ($this->{$attr} as $key => $value) {
-                $this->{$attr}[$key] = $this->makeOneParagraphs($value);
-            }
-        }
-        return $this;
-    }
-
-    private function makeOneParagraphs(?string $s): ?string
-    {
-        if ($s === null) {
-            return null;
-        }
-        $arr = mb_split("\n\n", $s);
-        foreach ($arr as $key => $value) {
-            $value = trim(implode(' ', mb_split("\\s+", $value)));
-            if ($value === '') {
-                unset($arr[$key]);
-            } else {
-                $arr[$key] = "<p>$value</p>";
-            }
-        }
-        return implode('', $arr);
     }
 }
