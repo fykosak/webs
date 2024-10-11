d402ab077e2bf3c86556c4105157d2dc44c647a0
Pavel Provazník <41260648+paproc@users.noreply.github.com>
2024-10-11 18:29:36 +0200

    latte lint

--- tests/lint-latte.php
+++ tests/lint-latte.php
@@ -3,25 +3,9 @@
 
 declare(strict_types=1);
 
-if (function_exists('pcntl_signal')) {
-    pcntl_signal(SIGINT, function (): void {
-        pcntl_signal(SIGINT, SIG_DFL);
-        echo "Terminated\n";
-        exit(1);
-    });
-} elseif (function_exists('sapi_windows_set_ctrl_handler')) {
-    sapi_windows_set_ctrl_handler(function () {
-        echo "Terminated\n";
-        exit(1);
-    });
-}
-
 set_time_limit(0);
 
-
-
 require __DIR__ . '/../app/Bootstrap.php';
-require __DIR__ . '/../vendor/autoload.php';
 
 $debug = in_array('--debug', $argv, true);
 
