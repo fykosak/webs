3fac759a8b67353ed4a36cc593696a073d70764d
Míra Jarý <jason@vyfuk.org>
2024-10-11 22:37:32 +0200

    ✨ Reorganize navbar

--- app/Modules/Vyfuk/Core/BasePresenter.php
+++ app/Modules/Vyfuk/Core/BasePresenter.php
@@ -17,31 +17,6 @@ abstract class BasePresenter extends \App\Modules\Core\BasePresenter
         $items = [];
 
         $items[] = new NavItem(
-            new PageTitle('O Nás'),
-            ':default',
-            [],
-            [
-                new NavItem(new PageTitle('Co je Výfuk?'), ':Default:About:default'),
-                new NavItem(new PageTitle('Historie'), ':Default:About:history'),
-                new NavItem(new PageTitle('Organizátoři'), ':Default:About:organizers'),
-                new NavItem(new PageTitle('Podpořte nás'), ':Default:About:sponsors'),
-                new NavItem(new PageTitle('Kontakt'), ':Default:About:contact'),
-            ],
-        );
-
-        $items[] = new NavItem(
-            new PageTitle('TODO název'), //TODO název sekce
-            ':default',
-            [],
-            [
-                new NavItem(new PageTitle('Pravidla'), ':Default:Section:rules'),
-                new NavItem(new PageTitle('Jak se zapojit'), ':Default:Section:howToEngage'),
-                new NavItem(new PageTitle('Pro učitele'), ':Default:Section:teachers'),
-                new NavItem(new PageTitle('Rady a tipy'), ':Default:Section:tips'),
-            ],
-        );
-
-        $items[] = new NavItem(
             new PageTitle('Zadání', 'bi bi-pencil-fill'),
             ':Default:Problems:default'
         );
@@ -49,6 +24,18 @@ abstract class BasePresenter extends \App\Modules\Core\BasePresenter
         $items[] = new NavItem(
             new PageTitle('Pořadí'),
             ':Default:Results:default'
+        );
+
+        $items[] = new NavItem(
+            new PageTitle('Jak řešit'),
+            ':default',
+            [],
+            [
+                new NavItem(new PageTitle('Jak se zapojit'), ':Default:Section:howToEngage'),
+                new NavItem(new PageTitle('Pravidla'), ':Default:Section:rules'),
+                new NavItem(new PageTitle('Jak psát řešení'), ':Default:Section:howToSolve'),
+                new NavItem(new PageTitle('Jak psát experimenty'), ':Default:Section:howToExperiment'),
+            ],
         );
 
         $items[] = new NavItem(
@@ -64,12 +51,20 @@ abstract class BasePresenter extends \App\Modules\Core\BasePresenter
         );
 
         $items[] = new NavItem(
-            new PageTitle('Archiv'),
+            new PageTitle('Pro učitele'),
+            ':Default:Section:teachers',
+        );
+
+        $items[] = new NavItem(
+            new PageTitle('O Nás'),
             ':default',
             [],
             [
-                new NavItem(new PageTitle('Úlohy'), ':Default:Archive:default'),
-                new NavItem(new PageTitle('Výfučtení'), ':Default:Archive:serials'),
+                new NavItem(new PageTitle('Co je Výfuk?'), ':Default:About:default'),
+                new NavItem(new PageTitle('Organizátoři'), ':Default:About:organizers'),
+                new NavItem(new PageTitle('Historie'), ':Default:About:history'),
+                new NavItem(new PageTitle('Podpořte nás'), ':Default:About:sponsors'),
+                new NavItem(new PageTitle('Kontakt'), ':Default:About:contact'),
             ],
         );
 
