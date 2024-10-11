adab8f4eacdecdeb0ebfe5b4d6548ff0bcca2e29
Pavel Provazník <41260648+paproc@users.noreply.github.com>
2024-10-11 18:49:45 +0200

    odstraněn kalendář

--- app/Modules/Vyfuk/DefaultModule/EventsPresenter.php
+++ app/Modules/Vyfuk/DefaultModule/EventsPresenter.php
@@ -6,10 +6,6 @@ namespace App\Modules\Vyfuk\DefaultModule;
 
 use App\Models\Downloader\EventService;
 use DateTime;
-use Exception;
-use Nette\Application\AbortException;
-use Nette\Application\BadRequestException;
-use Nette\Application\Responses\CallbackResponse;
 use Nette\Caching\Cache;
 use Nette\Caching\Storage;
 
@@ -64,44 +60,5 @@ class EventsPresenter extends BasePresenter
         $this->template->eventTypes = $eventTypes;
         $this->template->selected = $selectedId;
         $this->template->events = array_reverse($events);
-    }
-
-    /**
-     * @throws AbortException
-     * @throws BadRequestException
-     * @throws \Throwable
-     */
-    public function actionGetRawCalendar(): void
-    {
-        $calendar = $this->loadCalendar();
-        if (!$calendar) {
-            $this->error('', 503);
-        } else {
-            $this->sendResponse(new CallbackResponse(function ($request, $response) use ($calendar) {
-                $response->setHeader('Content-Type', 'text/calendar');
-                echo ($calendar);
-            }));
-        }
-    }
-
-    /**
-     * @throws \Throwable
-     */
-    private function loadCalendar(): string
-    {
-        return $this->cache->load(
-            'vyfuk.calender',
-            function (&$dependencies): string {
-                $dependencies[Cache::Expire] = time() + 24 * 60 * 60;
-                $curl = curl_init('https://drive.vyfuk.org/remote.php/dav/public-calendars/tLnfCNLzypBHHeEb?export');
-                curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-                $res = curl_exec($curl);
-                curl_close($curl);
-                if (curl_getinfo($curl)['http_code'] != 200) {
-                    throw new Exception('Vyfuk calendar failed to be downloaded');
-                }
-                return $res;
-            }
-        );
     }
 }
