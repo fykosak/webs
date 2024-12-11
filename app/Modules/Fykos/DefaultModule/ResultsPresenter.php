726d034b7021b683118ebd8f3d0cdd790b68eefc
vercah <vercaposta@seznam.cz>
2024-12-11 14:01:17 +0100

    bring back body unready

--- app/Modules/Fykos/DefaultModule/ResultsPresenter.php
+++ app/Modules/Fykos/DefaultModule/ResultsPresenter.php
@@ -22,17 +22,7 @@ class ResultsPresenter extends BasePresenter
         $this->template->year = $year;
         $this->template->contest = $this->getContest();
         $this->template->results = $this->downloader->download(new SeriesResultsRequest(1, $year));
-        // HACK BEFORE BODY-READY IS IMPLEMENTED
-        $this->template->results = $this->removeSeriesFromResults(2, $this->template->results);
     }
 
-    public function removeSeriesFromResults(int $seriesId, array $results): array
-    {
-        foreach (['FYKOS_1', 'FYKOS_2', 'FYKOS_3', 'FYKOS_4'] as $key) {
-            if (isset($results['tasks'][$key][$seriesId])) {
-                unset($results['tasks'][$key][$seriesId]);
-            }
-        }
-        return $results;
-    }
+
 }
