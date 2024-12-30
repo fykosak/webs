5d0bddfa8b815f5b443fcdd741c7a70a0e283b2d
Vojtěch Kubrycht <vojtakubrycht@seznam.cz>
2024-12-30 16:43:18 +0100

    rename variables

--- app/Components/ResultsVyfuk/results.tsx
+++ app/Components/ResultsVyfuk/results.tsx
@@ -28,18 +28,18 @@ type Tasks = {
 };
 
 function Results({ data, series }: { data: { submits: { [key: string]: Submits; }, tasks: { [key: string]: Tasks; } }, series: number[] }) {
-    const [selectedSerie, baseSetSelectedSerie] = useState(0);
+    const [selectedSeries, baseSetSelectedSeries] = useState(0);
     let tableManager: any = {};
-    [tableManager.sortColum, tableManager.setSortColum] = useState(selectedSerie < 1 ? "sum" : "s" + selectedSerie);
+    [tableManager.sortColum, tableManager.setSortColum] = useState(selectedSeries < 1 ? "sum" : "s" + selectedSeries);
     [tableManager.sortAsc, tableManager.setSortAsc] = useState(false);
     [tableManager.hideColum, tableManager.setHideColum] = useState({});
-    const setSelectedSerie = (value: number) => {
+    const setSelectedSeries = (value: number) => {
         tableManager.setSortColum(value < 1 ? "sum" : "s" + value);
-        baseSetSelectedSerie(value);
+        baseSetSelectedSeries(value);
     }
 
 
-    selectedSerie < 1 ? "sum" : "s" + selectedSerie
+    selectedSeries < 1 ? "sum" : "s" + selectedSeries
 
     const sortedCategories = Object.keys(data.submits).sort((a, b) => {
         return parseInt(b.slice(-1)) - parseInt(a.slice(-1));
@@ -47,12 +47,12 @@ function Results({ data, series }: { data: { submits: { [key: string]: Submits; 
     const sortedSeries = series.sort((a, b) => a - b);
 
     // category selection
-    let serieSelection = sortedSeries.map((number) => {
+    let seriesSelection = sortedSeries.map((number) => {
         if (number < 7)
             return (
                 <button
-                    className={`btn me-2 ${selectedSerie == number ? 'btn-primary' : 'btn-outline-primary'}`}
-                    onClick={() => setSelectedSerie(number)}
+                    className={`btn me-2 ${selectedSeries == number ? 'btn-primary' : 'btn-outline-primary'}`}
+                    onClick={() => setSelectedSeries(number)}
                 >
                     {number}. série
                 </button>
@@ -60,17 +60,17 @@ function Results({ data, series }: { data: { submits: { [key: string]: Submits; 
         else
             return (
                 <button
-                    onClick={() => setSelectedSerie(number)}
-                    className={`btn me-2 ${selectedSerie == number ? 'btn-primary' : 'btn-outline-primary'}`}
+                    onClick={() => setSelectedSeries(number)}
+                    className={`btn me-2 ${selectedSeries == number ? 'btn-primary' : 'btn-outline-primary'}`}
                 >
                     {number - 7}. prázdninová série
                 </button>
             )
     })
-    serieSelection.push(
+    seriesSelection.push(
         <button
-            onClick={() => setSelectedSerie(0)}
-            className={`btn me-2 ${selectedSerie == 0 ? 'btn-primary' : 'btn-outline-primary'}`}
+            onClick={() => setSelectedSeries(0)}
+            className={`btn me-2 ${selectedSeries == 0 ? 'btn-primary' : 'btn-outline-primary'}`}
         >
             Celkové výsledky
         </button>
@@ -79,7 +79,7 @@ function Results({ data, series }: { data: { submits: { [key: string]: Submits; 
         <div
             className='series-select my-2'
         >
-            {serieSelection}
+            {seriesSelection}
         </div>]
     for (const category of sortedCategories) {
         let tableDef: any = React.useMemo(() => {
@@ -88,15 +88,15 @@ function Results({ data, series }: { data: { submits: { [key: string]: Submits; 
             columns.push({ colKey: "rank", label: "#", sortable: false, numerical: false });
             columns.push({ colKey: "name", label: "Jméno", sortable: false, numerical: false });
             columns.push({ colKey: "school", label: "Škola", sortable: false, numerical: false });
-            for (let serie in data.tasks[category]) {
-                let tasks = data.tasks[category][serie].sort((a, b) => {
+            for (let series in data.tasks[category]) {
+                let tasks = data.tasks[category][series].sort((a, b) => {
                     return a.label.localeCompare(b.label);
                 });
                 for (let [key, t] of tasks.entries()) {
                     taskLockup[t.taskId] = columns.length;
-                    columns.push({ colKey: "s" + serie + "." + t.label, label: t.label + " (" + t.points + " b)", sortable: true, numerical: true });
+                    columns.push({ colKey: "s" + series + "." + t.label, label: t.label + " (" + t.points + " b)", sortable: true, numerical: true });
                 }
-                columns.push({ colKey: "s" + serie, label: (parseInt(serie) > 6 ? String(parseInt(serie) - 7) + ". p" : serie) + ". s.", sortable: true, numerical: true });
+                columns.push({ colKey: "s" + series, label: (parseInt(series) > 6 ? String(parseInt(series) - 7) + ". p" : series) + ". s.", sortable: true, numerical: true });
             }
             columns.push({ colKey: "sum", label: "Celkem\nbodů", sortable: true, numerical: true });
 
@@ -108,21 +108,21 @@ function Results({ data, series }: { data: { submits: { [key: string]: Submits; 
                 row["rank"] = contestant.rank[0] == contestant.rank[1] ? String(contestant.rank[0]) + "." : String(contestant.rank[0]) + ".–" + String(contestant.rank[1]) + ".";
 
                 let totalSum = 0;
-                for (let serie in data.tasks[category]) {
-                    let tasks = data.tasks[category][serie].sort((a, b) => {
+                for (let series in data.tasks[category]) {
+                    let tasks = data.tasks[category][series].sort((a, b) => {
                         return a.label.localeCompare(b.label);
                     });
-                    let serieSum = 0;
+                    let seriesSum = 0;
                     for (let [key, t] of tasks.entries()) {
                         if (contestant.submits.hasOwnProperty(t.taskId) && typeof contestant.submits[t.taskId] == 'number') {
-                            row["s" + serie + "." + t.label] = contestant.submits[t.taskId];
-                            serieSum += contestant.submits[t.taskId];
+                            row["s" + series + "." + t.label] = contestant.submits[t.taskId];
+                            seriesSum += contestant.submits[t.taskId];
                         } else {
-                            row["s" + serie + "." + t.label] = '–';
+                            row["s" + series + "." + t.label] = '–';
                         }
                     }
-                    row["s" + serie] = serieSum;
-                    totalSum += serieSum;
+                    row["s" + series] = seriesSum;
+                    totalSum += seriesSum;
                 }
                 row["sum"] = totalSum;
                 outData.push(row);
@@ -131,7 +131,7 @@ function Results({ data, series }: { data: { submits: { [key: string]: Submits; 
         }, [data, series]);
         let hidden: string[] = []
         let keys: string[] = tableDef.columns.map((col: ColumnDef) => { return col.colKey });
-        if (selectedSerie < 1) {
+        if (selectedSeries < 1) {
             hidden = keys.filter((v) => {
                 if (v == "name" || v == "school" || v == "sum" || v == "rank") return false;
                 return !/^s[0-9]$/.test(v);
@@ -139,7 +139,7 @@ function Results({ data, series }: { data: { submits: { [key: string]: Submits; 
         } else {
             hidden = keys.filter((v) => {
                 if (v == "name" || v == "school" || v == "sum") return false;
-                return !(new RegExp("s" + selectedSerie)).test(v);
+                return !(new RegExp("s" + selectedSeries)).test(v);
             })
         }
         tableManager.hideColum = hidden.reduce((prev: any, c) => {
