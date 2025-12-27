3395a999eff97e97fe9d0a769cfc2a99d8673b10
Michal Mišo Červeňák <miso@fykos.cz>
2025-12-27 14:22:38 +0100

    Event orgs

--- app/Modules/Fof/DefaultModule/AboutTheCompetitionPresenter.php
+++ app/Modules/Fof/DefaultModule/AboutTheCompetitionPresenter.php
@@ -4,6 +4,28 @@ declare(strict_types=1);
 
 namespace App\Modules\Fof\DefaultModule;
 
+use App\Models\Downloader\EventOrganizersRequest;
+use App\Models\Downloader\FKSDBDownloader;
+
 class AboutTheCompetitionPresenter extends BasePresenter
 {
+    private readonly FKSDBDownloader $downloader;
+
+    public function inject(FKSDBDownloader $downloader): void
+    {
+        $this->downloader = $downloader;
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    public function renderOrganizers(): void
+    {
+        $organizers = $this->downloader->download(new EventOrganizersRequest($this->getNewestEvent()->eventId));
+        usort(
+            $organizers,
+            fn(array $a, array $b) => $b['order'] <=> $a['order']
+        );
+        $this->template->organizers = $organizers;
+    }
 }
