b9bc163012d5bac63dcce055e1e7a08b66c318b2
Vojtěch Kubrycht <vojtakubrycht@seznam.cz>
2026-08-25 13:45:05 +0200

    create NewsService

---
+++ app/Models/Downloader/Services/NewsService.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models\Downloader\Services;
+
+use DateTime;
+use Nette\Caching\Storage;
+use Nette\DI\Container;
+use App\Models\Downloader\Models\News\NewsModel;
+
+final class NewsService extends AbstractJSONService
+{
+    public string $mediaDir;
+
+    public function __construct(
+        string $expiration,
+        Storage $storage,
+        Container $container
+    ) {
+        $this->mediaDir = $container->getParameter('mediaDir');
+        parent::__construct($expiration, $storage);
+    }
+
+    private function loadNews(): array
+    {
+        $json = file_get_contents($this->mediaDir . '/news.json');
+        return $this->mapJsonToClass($json, true, NewsModel::class);
+    }
+
+    private function saveNews(array $newsList): void
+    {
+        $json = json_encode($newsList);
+        file_put_contents($this->mediaDir . '/news.json', $json);
+    }
+
+    public function editNews(NewsModel $newsInput): void
+    {
+        $newsList = $this->loadNews();
+
+        $updatedNews = [];
+        /* TODO PHP 8.4 $news = array_find($newsList, fn($newsItem) => $newsItem['newsId'] === $news->newsId); */
+        foreach ($newsList as $newsItem) {
+            if (!$newsItem->newsId === $newsInput->newsId) {
+                $updatedNews[] = $newsItem;
+            }
+        }
+        $updatedNews[] = $newsInput;
+
+        $this->saveNews($updatedNews);
+    }
+
+    public function deleteNews(NewsModel $newsInput): void
+    {
+        $newsList = $this->loadNews();
+
+        $updatedNews = [];
+        /* TODO PHP 8.4 $news = array_find($newsList, fn($newsItem) => $newsItem['newsId'] === $news->newsId); */
+        foreach ($newsList as $newsItem) {
+            if (!$newsItem->newsId === $newsInput->newsId) {
+                $updatedNews[] = $newsItem;
+            }
+        }
+
+        $this->saveNews($updatedNews);
+    }
+
+    public function createNews(NewsModel $newsInput): void
+    {
+        $newsList = $this->loadNews();
+
+        $newsList[] = $newsInput;
+
+        $this->saveNews($newsList);
+    }
+
+    public function getActiveNews(int $number): array
+    {
+        $newsList = $this->loadNews;
+        usort($newsList, fn(NewsModel $a, NewsModel $b): int => $a->releaseDate <=> $b->releaseDate);
+
+        $activeNews = [];
+        $now = new DateTime();
+        foreach ($newsList as $newsItem) {
+            while (count($activeNews) <= $number) {
+                if ($now > $newsItem->releaseDate and !$newsItem->endDate || $now < $newsItem->endDate) {
+                    $activeNews[] = $newsItem;
+                }
+            }
+            break;
+        }
+
+        return $activeNews;
+    }
+}
