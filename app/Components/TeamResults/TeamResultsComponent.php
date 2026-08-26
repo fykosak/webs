c2f817b706d69f28603f4e764d69b67e18627376
Michal Mišo Červeňák <miso@fykos.cz>
2026-08-26 21:48:45 +0200

    fix PHPStan

--- app/Components/TeamResults/TeamResultsComponent.php
+++ app/Components/TeamResults/TeamResultsComponent.php
@@ -72,6 +72,7 @@ class TeamResultsComponent extends DIComponent
 
         // remove categories that are empty after the filtering
         foreach ($teams as $category => $teamsForCategory) {
+            /** @phpstan-ignore identical.alwaysFalse */
             if (count($teamsForCategory) === 0) {
                 unset($teams[$category]);
             }
@@ -177,6 +178,7 @@ class TeamResultsComponent extends DIComponent
                         Language::cs->value => ' %s: %s účastníků',
                         Language::en->value => ' %s: %s participants'
                     ]),
+                    /** @phpstan-ignore method.notFound */
                     $countryISO !== 'Uknown' ? $countryISO : $this->presenter->csen('Nestudent', 'Not a student'),
                     $count
                 )
